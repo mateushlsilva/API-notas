b4c11527d3ca582df5da5d9167415266dd6d621d
mateushlsilva <mateushls01@gmail.com>
2023-12-04 11:04:30 -0300

    feat: Adiciona getOne no controller aluno

--- src/controllers/Aluno.ts
+++ src/controllers/Aluno.ts
@@ -1,3 +1,4 @@
+import { ObjectId } from "typeorm";
 import AppDataSource from "../data-source";
 import { Aluno } from "../models";
 import { Request, Response } from "express";
@@ -7,11 +8,24 @@ class AlunoController{
         try{
             const rep = AppDataSource.getRepository(Aluno)
             const alunos = await rep.find()
-            return res.status(200).json({erro: false, message: "Alunos pegos com sucesso!", aluno: alunos})
+            return res.status(200).json({erro: false, message: "Alunos pegos com sucesso!", alunos: alunos})
         }catch(err){
             return res.status(400).json({erro: true, message: "Erro ao pegar os alunos!", tipoErro: err})
         }
     }
+
+    public async getOne(req: Request, res: Response): Promise<Response>{
+        try{
+            const id:any = new ObjectId(req.params.uuid)
+            const rep = AppDataSource.getMongoRepository(Aluno)
+            const find:any = await rep.findOneOrFail(id).catch((err) => {
+                return res.status(404).json({erro: true, message: "Aluno não existe!", tipoErro: err})
+            })
+        }catch(err){
+            return res.status(400).json({erro: true, message: "Erro ao pegar o aluno!", tipoErro: err})
+        }
+    }
+
 
     public async post(req: Request, res: Response): Promise<Response>{
         try{
