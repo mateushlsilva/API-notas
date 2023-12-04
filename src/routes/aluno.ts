3e850d6691fe98fe2648bb14377006eb69654533
mateushlsilva <mateushls01@gmail.com>
2023-12-04 13:24:43 -0300

    feat: Adiciona rota put do aluno

--- src/routes/aluno.ts
+++ src/routes/aluno.ts
@@ -6,5 +6,6 @@ const routes = Router()
 routes.get("/all", AlunoController.get)
 routes.post("/", AlunoController.post)
 routes.get("/one/:uuid", AlunoController.getOne)
+routes.put("/:uuid", AlunoController.put)
 
 export default routes
