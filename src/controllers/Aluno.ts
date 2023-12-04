8903266c1a4a2c5ae83ebf23fa1dd741f914424e
mateushlsilva <mateushls01@gmail.com>
2023-12-04 10:52:22 -0300

    feat: Adiciona post no controller aluno

--- src/controllers/Aluno.ts
+++ src/controllers/Aluno.ts
@@ -12,6 +12,21 @@ class AlunoController{
             return res.status(400).json({erro: true, message: "Erro ao pegar os alunos!", tipoErro: err})
         }
     }
+
+    public async post(req: Request, res: Response): Promise<Response>{
+        try{
+            const { cpf, ra, nome } = req.body
+            const rep = AppDataSource.getRepository(Aluno)
+            const aluno = new Aluno()
+            aluno.cpf = cpf 
+            aluno.nome = nome
+            aluno.ra = ra
+            await rep.save(aluno)
+            return res.status(200).json({erro: false, message: "Aluno cadastrado com sucesso!"})
+        }catch(err){
+            return res.status(400).json({erro: true, message: "Erro ao cadastrar novo aluno!", tipoErro: err})
+        }
+    }
 }
 
 export default new AlunoController()
